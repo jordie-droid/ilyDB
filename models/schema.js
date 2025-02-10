23e95189417fe7da0ec883ae0f0e26b074089e6d
jordie lutundula <redilutunx@gmail.com>
2025-02-10 21:49:45 +0100

    feat: ajout des systèmes de clés étrangères ou références d'objet

--- models/schema.js
+++ models/schema.js
@@ -4,22 +4,24 @@ class schema {
     properties,
     primaryKey = null,
     unique = [],
-    required = []
+    required = [],
+    foreignKeys = []
   ) {
     this.tableName = tableName;
     this.properties = properties;
     this.primaryKey = primaryKey;
     this.unique = unique;
     this.required = required;
+    this.foreignKeys = foreignKeys;
   }
 
-  validate(data) {
+  validate(data, relatedTables = {}) {
     const errors = [];
     this.required.forEach((key) => {
       if (!data.hasOwnProperty(key)) {
         errors.push(`Le champ "${key}" est requis.`);
       }
-    }); 
+    });
 
     for (let key in data) {
       const expectedType = this.properties[key];
@@ -27,6 +29,22 @@ class schema {
         errors.push(`Le champ "${key}" doit être de type "${expectedType}".`);
       }
     }
+
+    this.foreignKeys.forEach((foreignKey) => {
+      const { field, references } = foreignKey;
+      const relatedTable = relatedTables[references.table];
+      if (relatedTable) {
+        const validIds = relatedTable.map((row) => row[references.field]);
+        if (!validIds.includes(data[field])) {
+          errors.push(
+            `La valeur du champ "${field}" (${data[field]}) doit exister dans "${references.table}.${references.field}".`
+          );
+        }
+      } else {
+        errors.push(`Table de référence "${references.table}" introuvable.`);
+      }
+    });
+
     return errors.length ? errors : "Validation réussie !";
   }
 }
